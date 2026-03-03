43128c4d2e253b29b9de53e0054571797a74b347
Camila Holanda <cmdhn.holanda@gmail.com>
2026-03-03 09:29:21 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+function somar() {
+
+    let n1
+
+    n1 = prompt('Digite um número:')
+
+    n1 = parseInt(n1)
+
+    n2 = parseInt(prompt('Digite outro número:'))
+    
+    let res = n1 + n2
+    alert (`${n1} + ${n2} = ${res}`)
+    
+
+}
