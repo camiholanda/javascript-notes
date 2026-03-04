1b84f309d797adac378ce477b2cf0786683d97cd
Camila Holanda <cmdhn.holanda@gmail.com>
2026-03-04 13:04:31 -0300

    Add files via upload

---
+++ my-love-project.js
@@ -0,0 +1,5 @@
+function showLoveMessage() {
+    const messageElement = document.getElementById('love-message');
+    messageElement.textContent = 'Te amo, gatinho!';
+    messageElement.style.display = 'block';
+}
